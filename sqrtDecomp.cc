d6a22e9efe86076365dd12cbed653d1b11b6861e
yamate11 <yamate11@cent.xii.jp>
2025-05-18 14:42:28 +0900

    sqrtDecomp: interface change and performance

--- sqrtDecomp.cc
+++ sqrtDecomp.cc
@@ -16,10 +16,12 @@ using namespace std;
 
 template<typename B>
 struct SRD { // square root decomposition
+
   int tot_size;  // total number of elements
   int bsize;  // block size
   int numb;  // number of blocks
   vector<B> _data;
+
   SRD() : tot_size(0), bsize(0), numb(0), _data(0) {}
   SRD(int tot_size_, int bsize_ = 0) : tot_size(tot_size_) {
     if (bsize_ > 0) bsize = bsize_;
@@ -27,59 +29,46 @@ struct SRD { // square root decomposition
     numb = (tot_size + bsize - 1) / bsize;
     _data = vector<B>(numb);
   }
+
   pair<int, int> idx2pos(int x) { return {x / bsize, x % bsize}; }
   int pos2idx(int p, int r) { return p * bsize + r; }
+
   tuple<int, int, int, int, int, int> range_pos(int lo, int hi) {
     auto [lo_p, lo_r] = idx2pos(lo);
-    auto [hi_p, hi_r] = idx2pos(hi - 1);
-    if (lo_p == hi_p) return {lo_p, lo_r, hi_r + 1, hi_p, 0, 0};
-    else              return {lo_p, lo_r, bsize,    hi_p, 0, hi_r + 1};
+    auto [hi_p, hi_r] = idx2pos(hi);
+    ll block_start = lo_r == 0 ? lo_p : lo_p + 1;
+    ll block_end = hi_p;
+    ll pre_start = -1, pre_end = -1, post_start = -1, post_end = -1;
+    if (lo_r > 0 and hi_r > 0 and lo_p == hi_p) {
+      block_start = block_end;
+      post_start = lo;
+      post_end = hi;
+    }else {
+      if (lo_r > 0) {
+        pre_start = lo;
+        pre_end = pos2idx(lo_p + 1, 0);
+      }
+      if (hi_r > 0) {
+        post_start = pos2idx(hi_p, 0);
+        post_end = hi;
+      }
+    }
+    return {block_start, block_end, pre_start, pre_end, post_start, post_end};
   }
+
   B& data(int i) { return _data[i]; }
+
   int block_size(int i) {
     if (i < numb - 1) return bsize;
     else return tot_size - (numb - 1) * bsize;
   }
-  /*
-  void exec_general(int lo, int hi, auto f_edge, auto f_body) {
-    auto [b_f, lo_f, hi_f, b_l, lo_l, hi_l] = range_pos(lo, hi);
-    f_edge(b_f, lo_f, hi_f);
-    if (b_f + 1 < b_l) f_body(b_f + 1, b_l);
-    if (b_f < b_l) f_edge(b_l, lo_l, hi_l);
-  }
-  void exec(int lo, int hi, auto inloop_edge, auto inloop_body) {
-    auto f_edge = [&](int b, int lo_b, int hi_b) {
-      for (int i = lo_b; i < hi_b; i++) inloop_edge(b, data(b), i, pos2idx(b, i));
-    };
-    auto f_body = [&](int b0, int b1) {
-      for (int b = b0; b < b1; b++) inloop_body(b, data(b));
-    };
-    exec_general(lo, hi, f_edge, f_body);
-  }
-  */
-  template<typename F0e = nullptr_t, typename F0c = nullptr_t, typename F1e = nullptr_t, typename F1c = nullptr_t>
-  void exec(int lo, int hi, auto edge_body, auto core_body,
-            F0e edge_pre = nullptr, F0c core_pre = nullptr, F1e edge_post = nullptr, F1c core_post = nullptr) {
-    auto do_edge = [&](int b, int lo_e, int hi_e) -> void {
-      if constexpr (not is_same_v<F0e, nullptr_t>) { edge_pre(b, data(b)); }
-      if constexpr (not is_same_v<decltype(edge_body), nullptr_t>) {
-        for (int i = lo_e; i < hi_e; i++) { edge_body(b, data(b), i, pos2idx(b, i)); }
-      }
-      if constexpr (not is_same_v<F1e, nullptr_t>) { edge_post(b, data(b)); }
-    };
 
-    auto [b_f, lo_f, hi_f, b_l, lo_l, hi_l] = range_pos(lo, hi);
-    do_edge(b_f, lo_f, hi_f);
-    if (b_f + 1 < b_l) {
-      if constexpr (not is_same_v<F0c, nullptr_t>) { core_pre(); }
-      if constexpr (not is_same_v<decltype(core_body), nullptr_t>) {
-        for (int b = b_f + 1; b < b_l; b++) { core_body(b, data(b)); }
-      }
-      if constexpr (not is_same_v<F1c, nullptr_t>) { core_post(); }
-    }
-    if (b_f < b_l) do_edge(b_l, lo_l, hi_l);
+  void exec(int lo, int hi, auto f_edge, auto f_core) {
+    auto [block_start, block_end, pre_start, pre_end, post_start, post_end] = range_pos(lo, hi);
+    if (pre_start >= 0) for (int i = pre_start; i < pre_end; i++) f_edge(i, block_start - 1);
+    for (int b = block_start; b < block_end; b++) f_core(b);
+    if (post_start >= 0) for (int i = post_start; i < post_end; i++) f_edge(i, block_end);
   }
-
 
 };
 
--- test/sqrtDecomp_skel.cc
+++ test/sqrtDecomp_skel.cc
@@ -29,12 +29,12 @@ int main() {
     assert(srd1.idx2pos(7).second == 1);
     assert(srd1.pos2idx(1, 2) == 5);
     using rpr_t = decltype(srd1.range_pos(0, 0));
-    assert(srd1.range_pos(4, 5) == rpr_t(1, 1, 2, 1, 0, 0));
-    assert(srd1.range_pos(4, 6) == rpr_t(1, 1, 3, 1, 0, 0));
-    assert(srd1.range_pos(4, 7) == rpr_t(1, 1, 3, 2, 0, 1));
-    assert(srd1.range_pos(6, 7) == rpr_t(2, 0, 1, 2, 0, 0));
-    assert(srd1.range_pos(6, 9) == rpr_t(2, 0, 3, 2, 0, 0));
-    assert(srd1.range_pos(6, 10) == rpr_t(2, 0, 3, 3, 0, 1));
+    assert(srd1.range_pos(4, 5) == rpr_t(1, 1, -1, -1, 4, 5));
+    assert(srd1.range_pos(4, 6) == rpr_t(2, 2, 4, 6, -1, -1));
+    assert(srd1.range_pos(4, 7) == rpr_t(2, 2, 4, 6, 6, 7));
+    assert(srd1.range_pos(6, 7) == rpr_t(2, 2, -1, -1, 6, 7));
+    assert(srd1.range_pos(6, 9) == rpr_t(2, 3, -1, -1, -1, -1));
+    assert(srd1.range_pos(6, 10) == rpr_t(2, 3, -1, -1, 9, 10));
     S& s = srd1.data(1);
     s.a = 100;
     assert(srd1.data(1).a == 100);
@@ -51,39 +51,6 @@ int main() {
     assert(srd3.bsize == 100 and srd3.numb == 1);
     SRD<ll> srd4(100, 1000);
     assert(srd4.bsize == 1000 and srd4.numb == 1 and srd4.block_size(0) == 100);
-  }
-  { // exec arguments
-    SRD<ll> srd(10);
-    string ret;
-    auto edge_body = [&](ll b, ll& s, ll i, ll idx) { ret += (char)('0' + idx); };
-    auto core_body = [&](ll b, ll& s) { ret += (char)('A' + b); };
-    auto edge_pre = [&](ll b, ll& s) { ret += (char)('a' + b); };
-    auto core_pre = [&]() { ret += '@'; };
-    auto edge_post = [&](ll b, ll& s) { ret += (char)('m' + b); };
-    auto core_post = [&]() { ret += '$'; };
-    srd.exec(2, 7, edge_body, nullptr);
-    assert(ret == "26");
-    ret = "";
-
-    srd.exec(1, 3, edge_body, nullptr);
-    assert(ret == "12");
-    ret = "";
-
-    srd.exec(2, 7, nullptr, core_body);
-    assert(ret == "B");
-    ret = "";
-
-    srd.exec(2, 7, edge_body, core_body, edge_pre, core_pre, edge_post, core_post);
-    assert(ret == "a2m@B$c6o");
-    ret = "";
-
-    srd.exec(2, 7, edge_body, core_body, edge_pre, core_pre);
-    assert(ret == "a2@Bc6");
-    ret = "";
-
-    srd.exec(2, 7, edge_body, core_body, nullptr, core_pre);
-    assert(ret == "2@B6");
-    ret = "";
   }
   { // range sum query
     ll rep_num_1 = 200;
@@ -104,11 +71,12 @@ int main() {
         srd.data(b).sum += A[i];
       }
       auto modify = [&](ll lo, ll hi, ll a) {  // A[i] += a for i \in [lo, hi)
-        auto fe = [&](ll b, S& s, ll i, ll idx) {
+        auto fe = [&](ll idx, ll b) {
           A[idx] += a;
-          s.sum += a;
+          srd.data(b).sum += a;
         };
-        auto fb = [&](ll b, S& s) {
+        auto fb = [&](ll b) {
+          S& s = srd.data(b);
           s.sum += a * srd.block_size(b);
           s.shift += a;
         };
@@ -116,8 +84,8 @@ int main() {
       };
       auto get_sum = [&](ll lo, ll hi) -> ll { // returns sum { A[i] | i \in [lo, hi) }
         ll ret = 0;
-        auto fe = [&](ll b, S& s, ll i, ll idx) { ret += s.shift + A[idx]; };
-        auto fb = [&](ll b, S& s) { ret += s.sum; };
+        auto fe = [&](ll idx, ll b) { ret += srd.data(b).shift + A[idx]; };
+        auto fb = [&](ll b) { ret += srd.data(b).sum; };
         srd.exec(lo, hi, fe, fb);
         return ret;
       };
