3a37bf777481dca614c20dd94f0af201097a6d10
yamate11 <yamate11@cent.xii.jp>
2025-08-17 18:13:09 +0900

    smallVec

---
+++ smallVec.cc
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#include <cassert>
+typedef long long int ll;
+using namespace std;
+
+/*
+  Vector of small integers
+  https://yamate11.github.io/blog/posts/2025/08-17-smallvec/
+*/
+
+//////////////////////////////////////////////////////////////////////
+// See help of libins command for dependency spec syntax.
+// @@ !! BEGIN(unordered_map) ---- smallVec.cc
+
+template<int bits>
+struct small_vector_u64 {
+  u64 impl;
+  small_vector_u64(u64 impl_ = 0) : impl(impl_) {}
+  constexpr u64 mask() const { return (1ULL << bits) - 1; }
+
+  struct Subst {
+    small_vector_u64& x;
+    int i;
+    Subst(small_vector_u64& x_, int i_) : x(x_), i(i_) {}
+    Subst operator=(ll t) { x.set(i, t); return *this; }
+    operator ll() const { return x.at(i); }
+  };
+
+  ll set(ll i, ll t) {
+    impl &= ~(mask() << (i * bits));
+    impl |= t << (i * bits);
+    return t;
+  }
+
+  ll at(ll i) const { return (impl >> (i * bits)) & mask(); }
+  Subst rs(ll i) { return Subst(*this, i); }
+  ll operator[](ll i) const { return at(i); }
+  Subst operator[](ll i) { return rs(i); }
+
+  bool operator==(const small_vector_u64& o) const { return impl == o.impl; }
+  operator u64() const { return impl; }
+
+  string show(int len=-1) const {
+    string ret = "[";
+    if (len < 0) len = 64 / bits;
+    for (ll i = 0; i < len; i++) {
+      if (i > 0) ret += ", ";
+      ret += to_string(at(i));
+    }
+    ret += ']';
+    return ret;
+  }
+
+  friend ostream& operator<<(ostream& os, const small_vector_u64& vec) { return os << vec.show(); }
+};
+
+template <int bits>
+struct safe_custom_hash<small_vector_u64<bits>, void> {
+  size_t operator()(const small_vector_u64<bits>& x) const {
+    return safe_custom_hash<u64>{}(u64(x));
+  }
+};
+
+struct small_vector_string {
+  string impl;
+  small_vector_string() : impl() {}
+  small_vector_string(ll sz, ll init = 0) : impl(sz, (char)init) {}
+  small_vector_string(const string& impl_) : impl(impl_) {}
+  small_vector_string(string&& impl_) : impl(move(impl_)) {}
+
+  // We employ Subst struct so that "cerr << vec[0];" produces "0" rather than "^@".
+  struct Subst {
+    small_vector_string& x;
+    int i;
+    Subst(small_vector_string& x_, int i_) : x(x_), i(i_) {}
+    Subst operator=(ll t) { x.impl[i] = t; return *this; }
+    operator ll() const { return ll(x.impl[i]); }
+  };
+
+  ll operator[](ll i) const { return impl[i]; }
+  Subst operator[](ll i) { return Subst(*this, i); }
+
+  bool operator==(const small_vector_string& o) const { return impl == o.impl; }
+  operator string() const { return impl; }
+
+  string show() const {
+    string ret = "[";
+    for (ll i = 0; i < ssize(impl); i++) {
+      if (i > 0) ret += ", ";
+      ret += to_string(impl[i]);
+    }
+    ret += ']';
+    return ret;
+  }
+
+  friend ostream& operator<<(ostream& os, const small_vector_string& vec) { return os << vec.show(); }
+};
+
+template <>
+struct safe_custom_hash<small_vector_string, void> {
+  size_t operator()(const small_vector_string& x) const {
+    return safe_custom_hash<string>{}(string(x));
+  }
+};
+
+
+
+// @@ !! END ---- smallVec.cc
---
+++ test/smallVec_skel.cc
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+using ll = long long int;
+using pll = pair<ll, ll>;
+using u64 = unsigned long long;
+
+// @@ !! LIM(smallVec debug)
+
+int main() {
+
+  {
+    using myvec = small_vector_u64<4>;
+    myvec vec1;
+    for (ll i = 0; i < 16; i++) vec1[i] = i;
+    for (ll i = 0; i < 16; i++) {
+      assert(vec1[i] == i);
+    }
+    stringstream ss1;
+    ss1 << vec1;
+    vector<ll> vll1(16);
+    for (ll i = 0; i < 16; i++) vll1[i] = i;
+    stringstream ss2;
+    ss2 << vll1;
+    assert(ss1.str() == ss2.str());
+
+    myvec vec2, vec3, vec4;
+    vec2[1] = 13;
+    vec3[1] = 15;
+    vec4[1] = 13;
+    safe_umap<myvec, ll> mp;
+    mp[vec1] = 100;
+    mp[vec2] = 200;
+    mp[vec3] = 300;
+    auto it1 = mp.find(vec1);
+    auto it2 = mp.find(vec2);
+    auto it3 = mp.find(vec3);
+    auto it4 = mp.find(vec4);
+    assert(it4 != mp.end() and it4->second == 200);
+    vec4[1] = 20;
+    it4 = mp.find(vec4);
+    assert(it4 == mp.end());
+  }
+
+  {
+    using myvec = small_vector_u64<5>;
+    myvec vec1;
+    for (ll i = 0; i< 12; i++) vec1[i] = i;
+    stringstream ss1;
+    ss1 << vec1;
+    vector<ll> vll1(12);
+    for (ll i = 0; i < 12; i++) vll1[i] = i;
+    stringstream ss2;
+    ss2 << vll1;
+    assert(ss1.str() == ss2.str());
+  }
+
+  {
+    small_vector_string vec1(5);
+    vec1[1] = 15;
+    vec1[3] = 20;
+    vec1[2] = vec1[1] + vec1[3];
+    stringstream ss1;
+    ss1 << vec1;
+    assert(ss1.str() == "[0, 15, 35, 20, 0]");
+    stringstream ss2;
+    ss2 << vec1[3];
+    assert(ss2.str() == "20");
+
+    small_vector_string vec2(2);
+    vec2[1] = 13;
+
+    small_vector_string vec3(3);
+    vec3[1] = 13;
+
+    small_vector_string vec4(2);
+    vec4[1] = 5 + 8;
+
+    safe_umap<small_vector_string, ll> mp;
+    mp[vec1] = 100;
+    mp[vec2] = 200;
+    mp[vec3] = 300;
+    auto it = mp.find(vec4);
+    assert(it != mp.end() and it-> second == 200);
+    vec4[0] = 5;
+    it = mp.find(vec4);
+    assert(it == mp.end());
+  }
+
+
+  cerr << "ok." << endl;
+}
+
