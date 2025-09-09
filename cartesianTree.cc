c18490f0ba2ec90406a28a9e46c96dd170cc2d70
yamate11 <yamate11@cent.xii.jp>
2025-09-09 15:45:38 +0900

    documentation

--- cartesianTree.cc
+++ cartesianTree.cc
@@ -5,19 +5,7 @@ using namespace std;
 
 /*
   Cartesian Tree
-
-  Usage:
-    vector<ll> vec{80, 90, 20, 50, 50, 40}
-    CartesianTree cp(vec);
-    // CartesianTree cp(vec, less<ll>()); ... same
-    // CartesianTree cp(vec, [](ll a, ll b) { return a < b; }); ... same
-    // CartesianTree<ll> cp; cp.build(vec);  ... same
-    assert(cp.root == 2);     // vec[2] is the least element
-    assert(cp.left[2] == 0);  // least in vec[0:2] is vec[0]
-    assert(cp.left[0] == -1); // "no such element" is expressed by -1
-    assert(cp.right[2] == 5);
-    assert(cp.left[5] == 3 or cp.left[5] == 4);  // either of tied element can be chosen
-
+  https://yamate11.github.io/blog/posts/2025/09-09-cartesian-tree/
  */
 
 //////////////////////////////////////////////////////////////////////
