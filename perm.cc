d036162a805750376f751846c7ed91f7ed2530e6
yamate11 <yamate11@cent.xii.jp>
2025-12-05 23:15:42 +0900

    documentation

--- perm.cc
+++ perm.cc
@@ -4,70 +4,8 @@ typedef long long int ll;
 using namespace std;
 
 /*
-  Permutations
-
-  Structs:
-    IntPerm, IntComb, IntDupPerm, IntDupComb, IntDirProd, IntPartition
-
-  Usage:
-
-    IntPerm ip(3, 2);      // Sequences of length 2 from [0, 3), no duplication
-    IntComb ic(3, 2);      // Sorted sequences of length 2 from [0, 3), no duplication
-    IntDupPerm dip(3, 2);  // Sequences of length 2 from [0, 3)
-    IntDupComb dic(3, 2);  // Sorted sequences of length 2 from [0, 3)
-    IntDirProd did{vector{{1, 2, 3}}};  // Sequences (a, b, c) s.t. a < 1, b < 2 and c < 3
-    IntPartition part(5);  // Partitions of 5
-
-    while (ip.get()) {  
-      for (int i = 0; i < 2; i++) cout << ip[i] << " ";    // ip[i] or ip.at(i).  same results
-      // or, equivalently,
-      const vector<int>& v = ip.vec_view();
-      for (int i = 0; i < 2; i++) cout << v[i] << " ";
-    }
-
-    // Results:
-    //    IntPerm    ...       [0,1],[0,2],[1,0],      [1,2],[2,0],[2,1]
-    //    IntComb    ...       [0,1],[0,2],            [1,2]
-    //    IntDupPerm ... [0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]
-    //    IntDupComb ... [0,0],[0,1],[0,2],      [1,1],[1,2],            [2,2]
-    //
-    //    IntDirProd ... [0,0,0],[0,0,1],[0,0,2],[0,1,0],[0,1,1],[0,1,2]
-    //    IntPartition ... [[1,1,1,1,1],[1,1,1,2],[1,1,3],[1,2,2],[1,4],[2,3],[5]]
-
-    // Note:
-    //    Unless 0 <= n and 0 <= r, they returns {}.
-    //    IntPerm and IntComb returns {} unless r <= n.
-    //    When n = 0 and r = 0, IntDupPerm and IntDupComb returns { [] } instead of {},
-    //       which may not be appropriate (since they mean 0^0 = 1 and H(n, r) != C(n+r-1, r).)
-    //    When n = 0 and r != 0, all returns {}.
-
-    // When ip.get() returns false, it's internal has gone back to the
-    // init state, so you can continue the next round.
-
-   Results other than [0, 1, .., n -1]:
-
-     IntComb ip(4, 2, [100, 50, 90, 10]);  // [[100, 50], [100, 90], [100, 10], [50, 90], [50, 10], [90, 10]]
-
-     IntPerm<char> ip(3, 2, vector<char>{'a', 'b', 'c'});
-     // or, equivalently
-     IntPerm<char> ip(3, 2);
-     ip.set_mapping([](int i) -> "abc"[i]);
-
-   For Debugging:
-      const vector<int>& vec_view() const;
-
-   About partition:
-      table of partition numbers up to 10000: https://oeis.org/A000041/b000041.txt
-      // 0  1  2  3  4  5   6   7   8   9  10  11  12   13   14   15   16   17   18   19     //  i
-      [  1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490]    // p(i)
-      p(20) = 627
-      p(25) = 1958
-      p(30) = 5604
-      p(40) = 37338 (3.7E4)
-      p(50) = 204226 (2.0E5)
-      p(61) = 1121505 (1.1E6)
-      p(77) = 10619863 (1.1E7)
-
+  Permutation and the likes
+  https://yamate11.github.io/blog/posts/2023/02-28-perm-dup/
 */
 
 // @@ !! LIM()
