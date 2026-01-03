8e45b427eb1e199226f402b113634801f6d415a3
yamate11 <yamate11@cent.xii.jp>
2026-01-03 10:33:52 +0900

    FuncGraph:  Added repr() and changed some interfaces.

--- FuncGraph.cc
+++ FuncGraph.cc
@@ -17,12 +17,14 @@ using ll = long long int;
     fg.build();  // optional
 
     numcc = fg.numcc();        // the number of connected components
-    ccid = fg.ccid(i);         // the seq number of the connected component containing node i
-    sz1 = fg.cc_size(i);       // the size of the connected component containing i
-    sz2 = fg.cycle_size(i);    // the size of the cycle that is reachable from i
-    nd = fg.node_on_cycle(i);  // the first node on the cycle in the trajectory beginning with i
-    flag = fg.is_on_cycle(i);  // true if i is on a cycle
-    len = fg.dist_to_cycle(i);  // the distance to the cycle
+    c = fg.cc(nd);             // the id of the connected component containing node nd
+    nd = fg.repr(c);           // the representative node of connected component c.  nd is on a cycle.
+    sz1 = fg.cc_size(c);       // the size of connected component c
+    sz2 = fg.cycle_size(c);    // the size of the cycle of connected component c
+    nd1 = fg.node_on_cycle(nd);// the first node on the cycle in the trajectory beginning with nd
+    b = fg.is_on_cycle(nd);    // true if nd is on a cycle
+    d = fg.dist_to_cycle(nd);  // the distance to the cycle
+    vec = fg.bwd(nd);          // a vector of all predecessors of nd.  If nd is on a cycle, so is vec[0].
  */
 
 //////////////////////////////////////////////////////////////////////
@@ -32,18 +34,21 @@ using ll = long long int;
 struct FuncGraph {
   int size;
   vector<int> fwd;
+  vector<vector<int>> _bwd;
   vector<int> _cc;
+  vector<int> _repr;
   vector<int> _cc_size;
   vector<int> _cycle_size;
   vector<int> _on_cycle;
   vector<int> _dp;
   bool _built;
 
-  FuncGraph(int size_) : size(size_), fwd(size), _cc(size, -1), _cc_size(), _cycle_size(),
+  FuncGraph(int size_) : size(size_), fwd(size), _bwd(size), _cc(size, -1), _repr(), _cc_size(), _cycle_size(),
                          _on_cycle(size), _dp(size, -1), _built(false) {}
 
   void add_edge(int i, int j) {
     fwd[i] = j;
+    _bwd[j].push_back(i);   // _bwd[j][0] might be changed afterwards
   };
 
   int _set_depth(int i) {
@@ -66,11 +71,23 @@ struct FuncGraph {
           stack.push_back(k);
         }
         if (_cc[k] == tmpidx) {
+          _repr.push_back(k);
           _cc_size.push_back(stack.size());
           bool on = true;
           while (not stack.empty()) {
             int m = stack.back(); stack.pop_back();
-            _on_cycle[m] = on ? m : k;
+            if (on) {
+              _on_cycle[m] = m;
+              auto& v = _bwd[fwd[m]];
+              for (int p = 0; p < ssize(v); p++) {
+                if (v[p] == m) {
+                  if (p > 0) swap(v[0], v[p]);
+                  break;
+                }
+              }
+            }else {
+              _on_cycle[m] = k;
+            }
             if (m == k) {
               _cycle_size.push_back(_cc_size[tmpidx] - stack.size());
               on = false;
@@ -95,19 +112,24 @@ struct FuncGraph {
     return (ll)_cc_size.size();
   }
 
-  ll ccid(int i) {
+  ll cc(int i) {
     if (not _built) build();
     return _cc[i];
   }
 
-  ll cc_size(int i) {
+  ll repr(int c) {
     if (not _built) build();
-    return _cc_size[ccid(i)];
+    return _repr[c];
   }
 
-  ll cycle_size(int i) {
+  ll cc_size(int c) {
     if (not _built) build();
-    return _cycle_size[ccid(i)];
+    return _cc_size[c];
+  }
+
+  ll cycle_size(int c) {
+    if (not _built) build();
+    return _cycle_size[c];
   }
 
   ll node_on_cycle(int i) {
@@ -124,6 +146,12 @@ struct FuncGraph {
     if (not _built) build();
     return _dp[i];
   }
+
+  const vector<int>& bwd(int i) {
+    if (not _built) build();
+    return _bwd[i];
+  }
+
 
 };
 
--- test/FuncGraph_skel.cc
+++ test/FuncGraph_skel.cc
@@ -27,16 +27,16 @@ int main() {
     fg.add_edge(2, 2);
     fg.add_edge(3, 1);
     assert(fg.numcc() == 2);
-    assert(fg.ccid(0) == fg.ccid(1) and fg.ccid(1) == fg.ccid(3));
-    assert(fg.ccid(0) != fg.ccid(2));
-    assert(fg.cc_size(0) == 3);
-    assert(fg.cc_size(1) == 3);
-    assert(fg.cc_size(2) == 1);
-    assert(fg.cc_size(3) == 3);
-    assert(fg.cycle_size(0) == 2);
-    assert(fg.cycle_size(1) == 2);
-    assert(fg.cycle_size(2) == 1);
-    assert(fg.cycle_size(3) == 2);
+    assert(fg.cc(0) == fg.cc(1) and fg.cc(1) == fg.cc(3));
+    assert(fg.cc(0) != fg.cc(2));
+    assert(fg.cc_size(fg.cc(0)) == 3);
+    assert(fg.cc_size(fg.cc(1)) == 3);
+    assert(fg.cc_size(fg.cc(2)) == 1);
+    assert(fg.cc_size(fg.cc(3)) == 3);
+    assert(fg.cycle_size(fg.cc(0)) == 2);
+    assert(fg.cycle_size(fg.cc(1)) == 2);
+    assert(fg.cycle_size(fg.cc(2)) == 1);
+    assert(fg.cycle_size(fg.cc(3)) == 2);
     assert(fg.node_on_cycle(0) == 1);
     assert(fg.node_on_cycle(1) == 1);
     assert(fg.node_on_cycle(2) == 2);
@@ -49,6 +49,11 @@ int main() {
     assert(fg.dist_to_cycle(1) == 0);
     assert(fg.dist_to_cycle(2) == 0);
     assert(fg.dist_to_cycle(3) == 0);
+    for (int c = 0; c < fg.numcc(); c++) assert(fg.cc(fg.repr(c)) == c);
+    {                 assert(fg.bwd(0).empty()); }
+    { vector v{3, 0}; assert(fg.bwd(1) == v); }  // the order matters.
+    { vector v{2};    assert(fg.bwd(2) == v); }
+    { vector v{1};    assert(fg.bwd(3) == v); }
   }
   
   {
@@ -57,12 +62,12 @@ int main() {
     fg.add_edge(1, 0);
     fg.add_edge(2, 1);
     assert(fg.numcc() == 1);
-    assert(fg.cc_size(0) == 3);
-    assert(fg.cc_size(1) == 3);
-    assert(fg.cc_size(2) == 3);
-    assert(fg.cycle_size(0) == 2);
-    assert(fg.cycle_size(1) == 2);
-    assert(fg.cycle_size(2) == 2);
+    assert(fg.cc_size(fg.cc(0)) == 3);
+    assert(fg.cc_size(fg.cc(1)) == 3);
+    assert(fg.cc_size(fg.cc(2)) == 3);
+    assert(fg.cycle_size(fg.cc(0)) == 2);
+    assert(fg.cycle_size(fg.cc(1)) == 2);
+    assert(fg.cycle_size(fg.cc(2)) == 2);
     assert(fg.node_on_cycle(0) == 0);
     assert(fg.node_on_cycle(1) == 1);
     assert(fg.node_on_cycle(2) == 1);
@@ -72,6 +77,10 @@ int main() {
     assert(fg.dist_to_cycle(0) == 0);
     assert(fg.dist_to_cycle(1) == 0);
     assert(fg.dist_to_cycle(2) == 1);
+    for (int c = 0; c < fg.numcc(); c++) assert(fg.cc(fg.repr(c)) == c);
+    { vector v{1};    assert(fg.bwd(0) == v); }
+    { vector v{0, 2}; assert(fg.bwd(1) == v); }  // the order matters.
+    {                 assert(fg.bwd(2).empty()); }
   }
   
   {
@@ -97,12 +106,12 @@ int main() {
       ll nuf = 0; for (int i = 0; i < size; i++) if (uf.leader(i) == i) nuf++;
       assert(nuf == fg.numcc());
       for (int i = 0; i < size; i++) for (int j = i + 1; j < size; j++) {
-          assert((uf.leader(i) == uf.leader(j)) == (fg.ccid(i) == fg.ccid(j)));
+          assert((uf.leader(i) == uf.leader(j)) == (fg.cc(i) == fg.cc(j)));
         }
-      for (int i = 0; i < size; i++) assert(fg.cc_size(i) == uf.group_size(i));
+      for (int i = 0; i < size; i++) assert(fg.cc_size(fg.cc(i)) == uf.group_size(i));
       for (int i = 0; i < size; i++) {
         ll p = fg.dist_to_cycle(i);
-        ll c = fg.cycle_size(i);
+        ll c = fg.cycle_size(fg.cc(i));
         vector<int> traj(p + c);
         for (int j = 0, m = i; j < p + c; j++, m = fg.fwd[m]) traj[j] = m;
         for (int j = 0; j < p + c; j++) for (int k = j + 1; k < p + c; k++) assert(traj[j] != traj[k]);
@@ -110,6 +119,21 @@ int main() {
         assert(fg.node_on_cycle(i) == traj[p]);
         assert(fg.is_on_cycle(i) == (p == 0));
       }
+      for (int c = 0; c < fg.numcc(); c++) {
+        int i = fg.repr(c);
+        assert(fg.cc(i) == c);
+        assert(fg.is_on_cycle(i));
+      }
+      {
+        int s = 0;
+        for (int i = 0; i < size; i++) s += ssize(fg.bwd(i));
+        assert(s == size);
+        for (int i = 0; i < size; i++) {
+          for (ll j : fg.bwd(i)) assert(fg.fwd[j] == i);
+          if (fg.is_on_cycle(i)) assert(not fg.bwd(i).empty() and fg.is_on_cycle(fg.bwd(i)[0]));
+        }
+      }
+
     }
   }
 
