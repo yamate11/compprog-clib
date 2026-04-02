825278a6652b8de5ab2447a162fc706156e2a961
yamate11 <yamate11@cent.xii.jp>
2026-04-02 19:04:24 +0900

    sortedIndex2

--- sortedIndex.cc
+++ sortedIndex.cc
@@ -6,13 +6,29 @@ using namespace std;
 /*
   sortedIndex
 
-  sortedIndex(vec) returns a vector<int> p so that vec[p[0]], vec[p[1]], ..., vec[p[n-1]] is sorted.
+  * sortedIndex2(vec, comp) returns a pair (ord2idx, idx2ord) of vector<INT>:
+    - vec[ord2idx[0]], vec[ord2idx[1]], ..., vec[ord2idx[N-1]] is sorted.
+    - idx2ord is the reverse of ord2idx
+      Thus, vec[i] is the idx2ord[i]-th least element (zero-indexed).
+    - comp can be omitted.  The default is less<T>(), where T is the value type of vec.
+    - The default of INT is "long long".
 
-  Usage:
-    vector<string> vec = {"ttt", "zzz", "eee"};
-    auto p = sortedIndex(vec);                     // p == vector<int>({2, 0, 1})
-    auto q = sortedIndex(vec, greater<string>());  // q == vector<int>({1, 0, 2})
-    for (int i = 0; i < 3; i++) cout << vec[p[i]] << "\n";   // prints "eee", "ttt", "zzz"
+  * sortedIndex(vec, comp) returns ord2idx: it is equivallent to sortedIndex2(vec, comp).first.
+
+  Typical Usage:
+
+    vector<string> vec = {"bbb", "ccc", "aaa"};
+
+    auto [ord2idx, idx2ord] = sortedIndex2(vec);
+      // ord2idx == vector<INT>{2, 0, 1},   idx2ord == vector<INT>{1, 2, 0}
+    REP(ii, 0, N) {
+      ll i = ord2idx[ii];
+      // Now, vec[i] is the ii-th least element of vec.
+    }
+
+    auto ord2idx = sortedIndex(vec);   // Same as above if you do not need idx2ord.
+
+    auto ord2idx = sortedIndex(vec, greater<string>());   // reverse order
 
 */
 
@@ -20,14 +36,31 @@ using namespace std;
 // See help of libins command for dependency spec syntax.
 // @@ !! BEGIN() ---- sortedIndex.cc
 
-vector<int> sortedIndex(const auto& vec, auto comp) {
-  vector<int> ret(vec.size());
-  iota(ret.begin(), ret.end(), 0);
-  sort(ret.begin(), ret.end(), [&vec, &comp](int i, int j) { return comp(vec[i], vec[j]); });
+template<typename INT = long long int>
+vector<INT> sortedIndex(const auto& vec, auto comp) {
+  vector<INT> ret(vec.size());
+  iota(ret.begin(), ret.end(), (INT)0);
+  sort(ret.begin(), ret.end(), [&vec, &comp](INT i, INT j) { return comp(vec[i], vec[j]); });
   return ret;
 }
 
-template<typename Collection>
-vector<int> sortedIndex(const Collection& vec) { return sortedIndex(vec, less<typename Collection::value_type>()); }
+template<typename INT = long long int>
+pair<vector<INT>, vector<INT>> sortedIndex2(const auto& vec, auto comp) {
+  auto ord2idx = sortedIndex(vec, comp);
+  vector<INT> idx2ord(vec.size());
+  for (size_t i = 0; i < vec.size(); i++) idx2ord[ord2idx[i]] = i;
+  return {move(ord2idx), move(idx2ord)};
+}
+
+template<typename INT = long long int, typename Collection>
+vector<INT> sortedIndex(const Collection& vec) {
+  return sortedIndex<INT>(vec, less<typename Collection::value_type>());
+}
+
+template<typename INT = long long int, typename Collection>
+pair<vector<INT>, vector<INT>> sortedIndex2(const Collection& vec) {
+  return sortedIndex2<INT>(vec, less<typename Collection::value_type>());
+}
+
 
 // @@ !! END ---- sortedIndex.cc
--- test/sortedIndex_skel.cc
+++ test/sortedIndex_skel.cc
@@ -13,12 +13,47 @@ int main(int argc, char *argv[]) {
   cout << setprecision(20);
 
   {
-    vector<ll> vec1({10, 5, 7, 1, 20, 3});
-    auto p = sortedIndex(vec1);
-    assert(p == vector<int>({3, 5, 1, 2, 0, 4}));
-    auto q = sortedIndex(vec1, greater<ll>());
-    assert(q == vector<int>({4, 0, 2, 1, 5, 3}));
+    vector<ll> vec1{10, 5, 7, 1, 20, 3};
+    auto ord2idx_a = sortedIndex(vec1);
+    vector<ll> exp_a{3, 5, 1, 2, 0, 4};
+    assert(ord2idx_a == exp_a);
+    auto ord2idx_b = sortedIndex(vec1, greater<ll>());
+    vector<ll> exp_b{4, 0, 2, 1, 5, 3};
+    assert(ord2idx_b == exp_b);
+    auto [ord2idx_c, idx2ord_c] = sortedIndex2(vec1);
+    assert(ord2idx_c == exp_a);
+    vector<ll> exp_cio{4, 2, 3, 0, 5, 1};
+    assert(idx2ord_c == exp_cio);
+    auto [ord2idx_d, idx2ord_d] = sortedIndex2(vec1, greater<ll>());
+    assert(ord2idx_d == exp_b);
+    vector<ll> exp_dio{1, 3, 2, 5, 0, 4};
+    assert(idx2ord_d == exp_dio);
   }
+  {
+    deque<ll> vec1{10, 5, 7, 1, 20, 3};
+    auto ord2idx_a = sortedIndex(vec1);
+    vector<ll> exp_a{3, 5, 1, 2, 0, 4};
+    assert(ord2idx_a == exp_a);
+    auto ord2idx_b = sortedIndex(vec1, greater<ll>());
+    vector<ll> exp_b{4, 0, 2, 1, 5, 3};
+    assert(ord2idx_b == exp_b);
+    auto [ord2idx_c, idx2ord_c] = sortedIndex2(vec1);
+    assert(ord2idx_c == exp_a);
+    vector<ll> exp_cio{4, 2, 3, 0, 5, 1};
+    assert(idx2ord_c == exp_cio);
+    auto [ord2idx_d, idx2ord_d] = sortedIndex2(vec1, greater<ll>());
+    assert(ord2idx_d == exp_b);
+    vector<ll> exp_dio{1, 3, 2, 5, 0, 4};
+    assert(idx2ord_d == exp_dio);
+  }
+
+  {
+    vector<ll> vec1{10, 5, 7, 1, 20, 3};
+    auto ord2idx_a = sortedIndex<int>(vec1);
+    vector<int> exp_a{3, 5, 1, 2, 0, 4};
+    assert(ord2idx_a == exp_a);
+  }
+
   {
     vector<pll> vec1({{7, 3}, {7, 2}, {3, 0}, {4, 4}, {7, 10}, {1, 5}, {3, 2}});
     auto compare = [&](pll x, pll y) -> bool {
