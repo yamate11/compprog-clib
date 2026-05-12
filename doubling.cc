560366d319d63341cae716f102e9145d2cfa60b1
yamate11 <yamate11@cent.xii.jp>
2026-05-12 15:05:19 +0900

    doubling: reimplementation and binsearch

--- doubling.cc
+++ doubling.cc
@@ -9,141 +9,94 @@ using u64 = unsigned long long;
 */
 
 //////////////////////////////////////////////////////////////////////
-// See help of libins command for dependency spec syntax.
 // @@ !! BEGIN() ---- doubling.cc
+// https://github.com/yamate11/compprog-clib/blob/master/doubling.cc
 
-template<typename T = void, typename Add = void>
+template<typename INT, typename T = monostate, typename Prod_t = monostate>
 struct Doubling {
-  static constexpr bool HAS_T = not is_void_v<T>;
-  using Node = conditional_t<HAS_T, pair<int, T>, int>;
+  constexpr static bool hasT = not is_same_v<T, monostate>;
 
-  int n{};
-  vector<vector<Node>> tbl;
-  [[no_unique_address]] conditional_t<HAS_T, T, monostate> unit{};
-  [[no_unique_address]] conditional_t<HAS_T, Add, monostate> add{};
+  T unit{};                    // unit for monoid operation
+  Prod_t prod{};               // monoid operation
+  ll max_call = 0;             // the max value x for f^x
+  int num = 0;                 // f: [0, num) -> [0, num)
+  vector<vector<INT>> f_tbl{}; // f_tbl[k][i] = f^{2^k}(i)
+  vector<vector<T>> t_tbl{};   // t_tbl[k][i] = \prod_{j = 0}^{2^k - 1} m(f^{j}(i))
 
-  int& nd_int(Node& nd) { if constexpr (HAS_T) return nd.first; else return nd; }
-
-  void prepare_tbl(u64 lim, const auto& nxt) {
-    int K = 64 - countl_zero(lim);
-    tbl.resize(K, vector<Node>(n));
-    for (int i = 0; i < n; i++) nd_int(tbl[0][i]) = nxt[i];
-  }
-
-  void fill_tbl(u64 lim) {
-    for (int k = 0; k + 1 < ssize(tbl); k++) {
-      for (int i = 0; i < n; i++) {
-        nd_int(tbl[k + 1][i]) = nd_int(tbl[k][nd_int(tbl[k][i])]);
-        if constexpr (HAS_T) tbl[k + 1][i].second = add(tbl[k][i].second, tbl[k][tbl[k][i].first].second);
-      }
+  void prepare_tbl(ll max_call_, vector<INT> nxt, vector<T> mapping = vector<T>()) {
+    max_call = max_call_;
+    num = ssize(nxt);
+    int k0 = 64 - countl_zero((u64)max_call);
+    f_tbl.resize(k0, vector<INT>(num));
+    f_tbl[0] = move(nxt);
+    for (int k = 1; k < k0; k++) for (int i = 0; i < num; i++) f_tbl[k][i] = f_tbl[k - 1][f_tbl[k - 1][i]];
+    if constexpr (hasT) {
+      t_tbl.resize(k0, vector<T>(num));
+      t_tbl[0] = move(mapping);
+      for (int k = 1; k < k0; k++) for (int i = 0; i < num; i++) {
+          t_tbl[k][i] = prod(t_tbl[k - 1][i], t_tbl[k - 1][f_tbl[k - 1][i]]);
+        }
     }
   }
 
-  Doubling() {}
-
-  Doubling(ll lim, int n_, const auto& nxt) requires (not HAS_T) : n(n_) {
-    prepare_tbl(lim, nxt);
-    fill_tbl(lim);
-  }
-
-  template<typename U = T, typename A = Add> requires (HAS_T)
-  Doubling(ll lim, int n_, const auto& nxt, const auto& mapping, U unit_, A add_)
-    : n(n_), unit(unit_), add(add_) {
-    prepare_tbl(lim, nxt);
-    for (int i = 0; i < n; i++) tbl[0][i].second = mapping[i];
-    fill_tbl(lim);
-  }
-
-  Node val(ll x, int i) {
-    Node ret;
-    nd_int(ret) = i;
-    if constexpr (HAS_T) ret.second = unit;
-    for (int k = 0; x > 0; x >>= 1, k++) {
-      if (x & 1) {
-        Node cur = tbl[k][nd_int(ret)];
-        nd_int(ret) = nd_int(cur);
-        if constexpr (HAS_T) ret.second = add(ret.second, cur.second);
+  pair<INT, T> val_pair(ll r, int i) const {
+    int idx = i;
+    T ret{};
+    if constexpr (hasT) ret = unit;
+    for (int k = 0; r > 0; r >>= 1, k++) {
+      if (r & 1) {
+        if constexpr (hasT) ret = prod(ret, t_tbl[k][idx]);
+        idx = f_tbl[k][idx];
       }
     }
-    return ret;
+    return make_pair(idx, ret);
   }
 
-};
+  INT f_val(ll r, int i) const { return val_pair(r, i).first; }
+  T   t_val(ll r, int i) const { return val_pair(r, i).second; }
 
-template<typename T, typename Add>
-auto make_doubling_with_monoid_unit_add(ll lim, int n, const auto& nxt, const auto& mapping, T unit, Add add) {
-  return Doubling<T, decltype(add)>(lim, n, nxt, mapping, unit, add);
-}
-template<typename T>
-auto make_doubling_with_monoid(ll lim, int n, const auto& nxt, const auto& mapping) {
-  return make_doubling_with_monoid_unit_add(lim, n, nxt, mapping, T(), plus<T>());
-}
-
-
-
-/*
-struct DoublingFRel { // from functional relation
-  int n;
-  vector<vector<int>> tbl;
-
-  void _init(ll lim, auto frel) {
-    int K = 64 - countl_zero((u64)lim);
-    tbl.resize(K, vector<int>(n));
-    for (int i = 0; i < n; i++) tbl[0][i] = frel(i);
-    for (int k = 0; k + 1 < K; k++) for (int i = 0; i < n; i++) tbl[k + 1][i] = tbl[k][tbl[k][i]];
-  }
-
-  DoublingFRel(ll lim, int n_, auto frel) : n(n_) { _init(lim, frel); }
-  static DoublingFRel from_container(ll lim, int n, auto vec) {
-    return DoublingFRel(lim, n, [&](int i) { return vec[i]; });
-  }
-
-  int val(ll x, int i) { // Calculates frel^{(x)}(i).  Should be x <= lim.
-    for (int k = 0; x > 0; x >>= 1, k++) if (x & 1) i = tbl[k][i];
-    return i;
-  }
-};
-  
-template <typename T, typename add_t>
-struct DoublingCum {
-  const DoublingFRel& d;
-  T unit;
-  add_t add;
-  vector<vector<T>> tbl;
-
-  void _init(auto mapping) {
-    int K = d.tbl.size();
-    tbl.resize(K, vector<T>(d.n));
-    for (int i = 0; i < d.n; i++) tbl[0][i] = mapping(i);
-    for (int k = 0; k + 1 < K; k++) for (int i = 0; i < d.n; i++) tbl[k + 1][i] = add(tbl[k][i], tbl[k][d.tbl[k][i]]);
-  }
-
-  DoublingCum(const DoublingFRel& d_, auto mapping, T unit_, add_t add_)
-    : d(d_), unit(unit_), add(add_) { _init(mapping); }
-
-  T val(ll x, int i) { // the monoid sum of x objs from i.  i.e. from i to i + x - 1.
-    T ret = unit;
-    for (int k = 0; x > 0; x >>= 1, k++) if (x & 1) {
-        ret = add(ret, tbl[k][i]);
-        i = d.tbl[k][i];
+  ll binsearch_body(auto check, bool lo_exp, int i, ll lo, ll hi) const {
+    if (hi < 0) hi = max_call;
+    bool hi_checked = false;
+    auto [lo_idx, lo_val] = val_pair(lo, i);
+    bool start_check = check(lo_val);
+    if (lo_exp and not start_check) return lo - 1;
+    if (not lo_exp and start_check) return lo;
+    // DLOGKL("    ", lo, i, lo_idx, lo_val.left, lo_val.right, lo_val.numinc);
+    for (int h = ssize(f_tbl) - 1; h >= 0; h--) {
+      if (lo + (1LL << h) > hi) continue;
+      T v_inc = prod(lo_val, t_tbl[h][lo_idx]);
+      if (check(v_inc) == lo_exp) {
+        lo += 1LL << h;
+        lo_idx = f_tbl[h][lo_idx];
+        lo_val = v_inc;
+      }else {
+        hi = lo + (1LL << h);
+        hi_checked = true;
       }
-    return ret;
+    }
+    // DLOGKL("   .", lo, hi, lo_idx, lo_val.left, lo_val.right, lo_val.numinc, hi_checked);
+    if (lo_exp) return lo;
+    else if (hi_checked) return hi;
+    else return hi + 1;
   }
+  ll binsearch_lo(auto check, int i, ll lo = 0, ll hi = -1) const { return binsearch_body(check, true, i, lo, hi); }
+  ll binsearch_hi(auto check, int i, ll lo = 0, ll hi = -1) const { return binsearch_body(check, false, i, lo, hi); }
 };
 
-DoublingFRel doubling_from_func(ll lim, int n, auto func) { return DoublingFRel(lim, n, func); }
-DoublingFRel doubling_from_container(ll lim, int n, auto vec) {
-  return DoublingFRel(lim, n, [&vec](int i) { return vec[i]; });
+template<typename INT>
+auto make_doubling(ll max_call, vector<INT> nxt) {
+  Doubling<INT> d;
+  d.prepare_tbl(max_call, move(nxt));
+  return d;
 }
-template<typename T, typename add_t = std::plus<T>>
-auto doubling_cum_from_func(const DoublingFRel& d, auto mapping, T unit = T(), add_t add = plus<T>()) {
-  return DoublingCum<T, decltype(add)>(d, mapping, unit, add);
+
+template<typename INT, typename T, typename Prod_t>
+auto make_doubling(ll max_call, vector<INT> nxt, vector<T> mapping, T unit, Prod_t prod) {
+  Doubling<INT, T, decltype(prod)> d(unit, prod);
+  d.prepare_tbl(max_call, move(nxt), mapping);
+  return d;
 }
-template<typename T, typename add_t = std::plus<T>>
-auto doubling_cum_from_container(const DoublingFRel& d, auto vec_mapping, T unit = T(), add_t add = plus<T>()) {
-  return DoublingCum<T, decltype(add)>(d, [&vec_mapping](int i) { return vec_mapping[i]; }, unit, add);
-}
-*/
 
 // @@ !! END ---- doubling.cc
 
--- test/doubling_skel.cc
+++ test/doubling_skel.cc
@@ -12,7 +12,7 @@ using pll = pair<ll, ll>;
 #define REPOUT(i, a, b, exp, sep) REP(i, (a), (b)) { if (i != (a)) cout << (sep); cout << (exp); } cout << "\n"
 using u64 = unsigned long long;
 
-// @@ !! LIM(doubling random mod debug)
+// @@ !! LIM(debug doubling random mod)
 
 int main(/* int argc, char *argv[] */) {
   ios_base::sync_with_stdio(false);
@@ -21,19 +21,24 @@ int main(/* int argc, char *argv[] */) {
   
   Random rand;
 
+
   { // interfaces
-    Doubling dobj(100, 4, vector<int>{1, 2, 3, 0});
-    assert(dobj.val(13, 0) == 1);
-    Doubling<int, plus<int>> dobj2(100, 4, vector<int>{1, 2, 3, 0}, vector<int>{0, 0, 0, 1}, 0, plus<int>());
-    auto [i2, x2] = dobj2.val(13, 0);
+    auto dobj = make_doubling(100, vector<int>{1, 2, 3, 0});
+    assert(dobj.f_val(13, 0) == 1);
+
+    auto dobj2 = make_doubling(100, vector<ll>{1, 2, 3, 0}, vector<ll>{0, 0, 0, 1}, 0LL, plus<ll>());
+    auto [i2, x2] = dobj2.val_pair(13, 0);
     assert(i2 == 1 and x2 == 3);
-    ll big = 1e18;
-    auto dobj3 = make_doubling_with_monoid_unit_add(big, 4, vector<int>{1, 2, 3, 0}, vector<ll>{0, 0, 0, 1}, 0LL, plus<ll>());
-    auto [i3, x3] = dobj3.val(big, 0);
+
+    ll big = 1LL << 60;
+    auto dobj3 = make_doubling(big, vector<int>{1, 2, 3, 0}, vector<ll>{0, 0, 0, 1}, 0LL, plus<ll>());
+    auto [i3, x3] = dobj3.val_pair(big, 0);
     assert(i3 == 0 and x3 == big / 4);
-    auto dobj4 = make_doubling_with_monoid<ll>(big, 4, vector<int>{1, 2, 3, 0}, vector<ll>{0, 0, 0, 1});
-    auto [i4, x4] = dobj4.val(big - 1, 2);
+    auto [i33, x33] = dobj3.val_pair(big - 1, 0);
+    assert(i33 == 3 and x33 == x3 - 1);
+    auto [i4, x4] = dobj3.val_pair(big - 1, 2);
     assert(i4 == 1 and x4 == big / 4);
+    assert(dobj3.f_val(big - 1, 2) == 1 and dobj3.t_val(big - 1, 2) == big / 4);
   }
 
   { // ABC179-E  
@@ -41,41 +46,79 @@ int main(/* int argc, char *argv[] */) {
     auto func = [&](ll N, ll K, const vll& A) -> ll {
       vector<int> vf(N);
       REP(i, 0, N) vf[i] = (i + A[i]) % N;
-      auto dobj = make_doubling_with_monoid<ll>(K, N, vf, A);
-      return dobj.val(K, 0).second;
+      auto dobj = make_doubling(K, vf, A, 0LL, plus<ll>());
+      return dobj.t_val(K, 0);
     };
     assert(func(5, 3, vll({2, 1, 6, 3, 1})) == 11);
     assert(func(10, 1000000000000, vll({260522, 914575, 436426, 979445, 648772, 690081, 933447, 190629, 703497, 47202})));
   }
 
+  { // Non-commutative product.  AWC-0060E 
+    
+    using Fp = FpG<0>;
+    auto solve = [&](ll M, const auto& D, const auto& P, const auto& S, const auto& K) {
+      Fp::setMod(M);
+      ll lim = 1LL << 60;
+      using sta = pair<Fp, Fp>;
+      vector<sta> DD(ssize(D));
+      REP(i, 0, ssize(D)) DD[i] = sta(Fp(D[i]), Fp(10));
+      auto prod = [&](sta x, sta y) -> sta {
+        auto [tx, lx] = x;
+        auto [ty, ly] = y;
+        return sta(tx * ly + ty, lx * ly);
+      };
+      auto PP = P;
+      for (int& p : PP) p--;
+      auto dobj = make_doubling(lim, PP, DD, sta(Fp(0), Fp(1)), prod);
+      vector<Fp> ret(ssize(S));
+      REP(i, 0, ssize(S)) ret[i] = dobj.t_val(K[i], S[i] - 1).first;
+      return ret;
+    };
+    auto a1 = solve(1000, vector{1, 2, 3, 4}, vector{2, 3, 4, 2}, vector{1, 2, 4, 3}, vector{3, 5, 4, 0});
+    auto e1 = vector<Fp>{123, 423, 234, 0};
+    auto a2 = solve(13, vector{0, 5, 9}, vector{2, 2, 1}, vector{1, 1, 3, 2, 3}, vector{1, 3, 2, 4, 0});
+    auto e2 = vector<Fp>{0, 3, 12, 4, 0};
+    auto a3 = solve(257, vector{6,2,0,7,5,1,9,3,4}, vector{2,3,4,2,3,5,8,9,7},
+                    vector{1,1,6,5,7,8,9,3}, vector{4,10,6,0,5,12,1,100});
+    auto e3 = vector<Fp>{39,58,118,0,202,176,4,110};
+    assert(a1 == e1);
+    assert(a2 == e2);
+    assert(a3 == e3);
+  }
+
   {
-    ll n = 10;
-    ll K = 100;
-    ll rep = 1000;
+    ll n0 = 10;
+    ll K0 = 20;
+    ll rep = 10000;
 
     { // function only
       REP(_r, 0, rep) {
+        ll n = rand.range(1, n0);
+        ll K = rand.range(1, K0);
         vector<ll> A(n);
         REP(i, 0, n) A[i] = rand.range(0, n);
-        Doubling d(K, n, A);
+        auto d = make_doubling(K, A);
         auto naive_f = [&](ll r, ll x) -> ll {
           REP(i, 0, r) x = A[x];
           return x;
         };
         REP(i, 0, n) {
           ll r = rand.range(0, K + 1);
-          assert(d.val(r, i) == naive_f(r, i));
+          assert(d.f_val(r, i) == naive_f(r, i));
         }
       }
     }
+
     { // monoid: Fp
       using Fp = FpB;
       REP(_r, 0, rep) {
+        ll n = rand.range(1, n0);
+        ll K = rand.range(1, K0);
         vector<ll> A(n);
         REP(i, 0, n) A[i] = rand.range(0, n);
         vector<Fp> B(n);
         REP(i, 0, n) B[i] = Fp(rand.range(0, (ll)1e9));
-        auto naive_m = [&](ll r, ll x) -> pair<int, Fp> {
+        auto naive_m = [&](ll r, ll x) -> pair<ll, Fp> {
           Fp v = 0;
           REP(i, 0, r) {
             v += B[x];
@@ -83,16 +126,19 @@ int main(/* int argc, char *argv[] */) {
           }
           return {x, v};
         };
-        auto d = make_doubling_with_monoid<Fp>(K, n, A, B);
+        auto d = make_doubling(K, A, B, Fp(0), plus<Fp>());
         REP(i, 0, n) {
           ll r = rand.range(0, K + 1);
-          assert(d.val(r, i) == naive_m(r, i));
+          assert(d.val_pair(r, i) == naive_m(r, i));
         }
       }
     }
+
     { // monoid: min
       REP(_r, 0, rep) {
-        vector<ll> A(n);
+        ll n = rand.range(1, n0);
+        ll K = rand.range(1, K0);
+        vector<int> A(n);
         REP(i, 0, n) A[i] = rand.range(0, n);
         vector<ll> B(n);
         REP(i, 0, n) B[i] = rand.range(0, (ll)1e9);
@@ -105,14 +151,85 @@ int main(/* int argc, char *argv[] */) {
           return {x, v};
         };
         auto mymin = [](ll a, ll b) -> ll { return min(a, b); };
-        auto d = make_doubling_with_monoid_unit_add(K, n, A, B, (ll)1e9, mymin);
+        auto d = make_doubling(K, A, B, (ll)1e9, mymin);
         REP(i, 0, n) {
           ll r = rand.range(0, K + 1);
-          assert(d.val(r, i) == naive_m(r, i));
+          assert(d.val_pair(r, i) == naive_m(r, i));
         }
       }
     }
+
+    { // binsearch
+
+      struct S {
+        ll left;
+        ll right;
+        ll numinc;
+        ostream& operator<<(ostream& ostr) const {
+          return ostr << "(" << this->left << ", " << this->right << ", " << this->numinc << ")";
+        }
+        auto operator<=>(const S&) const = default;
+      };
+
+      REP(_r, 0, rep) {
+        ll n = rand.range(1, 10);
+        ll R = rand.range(1, 20);
+        ll K = rand.range(1, 10);
+        vector<int> A(n);
+        REP(i, 0, n) A[i] = rand.range(0, n);
+        S unit_S{-1, -1, 0};
+        vector<S> M(n);
+        REP(i, 0, n) M[i] = S{i, i, 0};
+        auto prod = [&](const S& a, const S&b) -> S {
+          if (a == unit_S) return b;
+          if (b == unit_S) return a;
+          return S{a.left, b.right, a.numinc + b.numinc + (a.right < b.left ? 1 : 0)};
+        };
+        auto naive_1 = [&](ll x, ll lo, ll hi) -> ll {
+          auto op = [&](int i, const S& a) -> pair<int, S> {
+            return make_pair(A[i], prod(a, S{i, i, 0}));
+          };
+          int idx = x;
+          S acc = unit_S;
+          REP(i, 0, lo) {
+            tie(idx, acc) = op(idx, acc);
+            DLOGKL("  ", i, idx, acc.numinc);
+          }
+          if (acc.numinc >= K) return lo;
+          REP(i, lo, hi) {
+            tie(idx, acc) = op(idx, acc);
+            DLOGKL("  ", i, idx, acc.numinc);
+            if (acc.numinc >= K) return i + 1;
+          }
+          return hi + 1;
+        };
+        auto naive_2 = [&](ll x, ll lo, ll hi) -> ll { return naive_1(x, lo, hi) - 1; };
+        auto d = make_doubling(R, A, M, unit_S, prod);
+        DLOGK(A, R, K);
+        REP(_rr, 0, 5) {
+          auto [lo1, hi1] = rand.select2(0, R + 2); hi1--;
+          ll x1 = rand.range(0, n);
+          ll a1 = naive_1(x1, lo1, hi1);
+          ll b1 = d.binsearch_hi([&](const S& s) { return s.numinc >= K; }, x1, lo1, hi1);
+          DLOGK(a1, b1, x1, lo1, hi1);
+          assert(a1 == b1);
+          auto [lo2, hi2] = rand.select2(0, R + 2); hi2--;
+          ll x2 = rand.range(0, n);
+          ll a2 = naive_2(x2, lo2, hi2);
+          ll b2 = d.binsearch_lo([&](const S& s) { return s.numinc < K; }, x2, lo2, hi2);
+          DLOGK(a2, b2, x2, lo2, hi2);
+          assert(a2 == b2);
+          ll x3 = rand.range(0, n);
+          ll a3 = naive_1(x3, 0, R);
+          ll b3 = d.binsearch_hi([&](const S& s) { return s.numinc >= K; }, x3);
+          DLOGK(a3, b3, x3);
+          assert(a3 == b3);
+        }
+      }
+    }
+
   }
+
   
   cout << "ok" << endl;
   return 0;
