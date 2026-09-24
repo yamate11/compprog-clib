607891628daa6fdb173d8af5988945eb3fad3f77
yamate11 <yamate11@cent.xii.jp>
2026-09-24 22:30:09 +0900

    Li Chao tree supports add_segment

--- LiChao.cc
+++ LiChao.cc
@@ -6,9 +6,13 @@
 // See help of libins command for dependency spec syntax.
 // @@ !! BEGIN() ---- LiChao.cc
 
-// T must be integral or floating
+// T must be integral
 template<typename T = long long, typename comp_tp = less<T>>
-requires (std::integral<T> || std::floating_point<T>)
+/*
+// requires (std::integral<T> || std::floating_point<T>)
+... seems difficult to support segments for floating-point
+*/
+requires (std::integral<T>)
 struct LiChaoDyn {
   struct Line {
     T a{};
@@ -28,12 +32,20 @@ struct LiChaoDyn {
   T range_max;
   comp_tp comp;
   Node* root{};
+  T _unit = T{};
+  bool _has_unit_set{false};
   
-  LiChaoDyn(T rmin, T rmax, comp_tp comp_ = comp_tp()) : range_min(rmin), range_max(rmax), comp(comp_) {}
+  LiChaoDyn(type_identity_t<T> rmin,      // so that T cannot be deduced from rmin
+            type_identity_t<T> rmax,      // so that T cannot be deduced from rmax
+            comp_tp comp_ = comp_tp()) : range_min(rmin), range_max(rmax), comp(comp_) {}
 
   T _better(T t1, T t2) const { return comp(t1, t2) ? t1 : t2; };
 
+  T _get_midL(T lo, T hi) const { return lo + (hi - lo) / 2; } // beware lo + hi can be negative
+  T _get_midH(T lo, T hi) const { return _get_midL(lo, hi) + 1; }
+
   void _sub_add_line(Node*& p, T lo, T hi, Line y) {
+    // DLOGKL("_sub_add_line", lo, hi, y.a, y.b);
     if (not p) p = new Node(y);
     else {
       bool r_lo  = comp(p->val_at(lo), y.val_at(lo));
@@ -41,15 +53,16 @@ struct LiChaoDyn {
       if (r_lo and r_hi) ; // nothing to do
       else if (not r_lo and not r_hi) p->line = y;
       else {
-        T mid = (lo + hi) / 2;
-        bool r_mid = comp(p->val_at(mid), y.val_at(mid));
+        T midL = _get_midL(lo, hi);
+        T midH = _get_midH(lo, hi);
+        bool r_mid = comp(p->val_at(midL), y.val_at(midL));
         if (not r_mid) {
           swap(p->line, y);
           r_lo = not r_lo;
           r_hi = not r_hi;
         }
-        if (not r_lo)      _sub_add_line(p->cldL, lo, mid, y);
-        else if (not r_hi) _sub_add_line(p->cldH, mid, hi, y);
+        if (not r_lo)      _sub_add_line(p->cldL, lo, midL, y);
+        else if (not r_hi) _sub_add_line(p->cldH, midH, hi, y);
         else assert(0);
       }
     }
@@ -59,19 +72,48 @@ struct LiChaoDyn {
   T _sub_query(Node* p, T lo, T hi, T t) const {
     if (not p) { throw runtime_error("LiChaoDyn.query: null pointer"); }
     T thisval = p->val_at(t);
-    T mid = (lo + hi) / 2;
-    if (t < mid) return p->cldL ? _better(thisval, _sub_query(p->cldL, lo, mid, t)) : thisval;
-    else         return p->cldH ? _better(thisval, _sub_query(p->cldH, mid, hi, t)) : thisval;
+    if (lo == hi) return thisval;
+    T midL = _get_midL(lo, hi);
+    if (t <= midL) return p->cldL ? _better(thisval, _sub_query(p->cldL, lo, _get_midL(lo, hi), t)) : thisval;
+    else           return p->cldH ? _better(thisval, _sub_query(p->cldH, _get_midH(lo, hi), hi, t)) : thisval;
   }
   T query(T t) const { return _sub_query(root, range_min, range_max, t); }
+
+  void set_unit(T _unit_) {
+    // DLOGKL("set_unit is called", _unit_);
+    _unit = _unit_;
+    _has_unit_set = true;
+    // DLOGKL("in set_unit", _unit);
+  }
+
+  void _sub_add_segment(Node*& p, T lo, T hi, T lo_segm, T hi_segm, Line y) {
+    // DLOGKL("_sub_add_segment", lo, hi, lo_segm, hi_segm, y.a, y.b);
+    if (hi < lo_segm or hi_segm < lo) ; // nothing to do 
+    else if (lo_segm <= lo and hi <= hi_segm) _sub_add_line(p, lo, hi, y);
+    else {
+      if (not p) {
+        // DLOGK(_unit);
+        p = new Node(Line{T{}, _unit});
+      }
+      ll midL = _get_midL(lo, hi);
+      ll midH = _get_midH(lo, hi);
+      _sub_add_segment(p->cldL, lo, midL, lo_segm, hi_segm, y);
+      _sub_add_segment(p->cldH, midH, hi, lo_segm, hi_segm, y);
+    }
+  }
+  void add_segment(T a, T b, T lo_segm, T hi_segm) {
+    if (not _has_unit_set) throw runtime_error("LChaoDyn.add_segment: set_unit() has not been called.");
+    _sub_add_segment(root, range_min, range_max, lo_segm, hi_segm, Line{a, b}); 
+  }
 
 #if LIB_DEBUG
   string innerstr() const {
     stringstream ss;
     ll seq = 0;
     map<Node*, ll> mp;
-    auto f = [&](auto rF, Node* p) -> void {
-      ss << "[" << mp[p] << "]  y = " << p->line.a << " x + " << p->line.b << "  ";
+    auto f = [&](auto rF, Node* p, ll lo, ll hi) -> void {
+      ss << "{" << mp[p] << "}  y = " << p->line.a << " x + " << p->line.b << "  ";
+      ss << "[" << lo << ", " << hi << "]  ";
       auto g = [&](Node* q) -> void {
         if (q) {
           ss << "(" << seq << ")  ";
@@ -84,11 +126,11 @@ struct LiChaoDyn {
       g(p->cldL);
       g(p->cldH);
       ss << "\n";
-      if (p->cldL) rF(rF, p->cldL);
-      if (p->cldH) rF(rF, p->cldH);
+      if (p->cldL) rF(rF, p->cldL, lo, _get_midL(lo, hi));
+      if (p->cldH) rF(rF, p->cldH, _get_midH(lo, hi), hi);
     };
     mp[root] = seq++;
-    f(f, root);
+    f(f, root, range_min, range_max);
     return ss.str();
   }
 #endif // LIB_DEBUG
--- test/LiChao_skel.cc
+++ test/LiChao_skel.cc
@@ -34,37 +34,84 @@ int main() {
     assert(lctG.query(0) == 20);
     assert(lctG.query(100) == 220);
   }
+  {
+    ll big = 1LL << 60;
+    LiChaoDyn lct(-200, 200);
+    // DLOGKL("just before calling set_unit", big);
+    lct.set_unit(big);
+    lct.add_segment(1, 0, -100, 100);
+    lct.add_segment(1, -1, 20, 30);
+    lct.add_segment(-1, -10, -6, -4);
+    // DLOG(lct.innerstr());
+    assert(lct.query(100) == 100);
+    assert(lct.query(101) == big);
+    // DLOGK(lct.query(0));
+    assert(lct.query(0) == 0);
+    assert(lct.query(19) == 19);
+    assert(lct.query(20) == 19);
+    assert(lct.query(25) == 24);
+    assert(lct.query(30) == 29);
+    assert(lct.query(31) == 31);
+    assert(lct.query(-4) == -6);
+    assert(lct.query(-5) == -5);
+    assert(lct.query(-6) == -6);
+  }
+  {
+    LiChaoDyn lct(-100, 100);
+    try {
+      lct.add_segment(1, 0, -10, 10);
+      // calling add_segment without set_unit should be an error.
+      assert(0);
+    }catch (runtime_error& e) {
+    }
+  }
 
   {
-    auto func = [&rand](auto comp) -> void {
+    ll big = 1LL << 60;
+    auto func = [&rand](auto comp, ll unit) -> void {
 #if DEBUG
       ll rep1 = 100, rep2 = 100;
 #else
-      ll rep1 = 500, rep2 = 500;
+      ll rep1 = 400, rep2 = 400;
+      // ll rep1 = 2000, rep2 = 2000;
 #endif
       for (int _r = 0; _r < rep1; _r++) {
         ll rngmin = rand.range(-rep2 * 2, rep2 * 2);
         ll rngmax = rngmin + rand.range(1, rep2 * 2);
         LiChaoDyn<ll, decltype(comp)> lct(rngmin, rngmax, comp);
+        lct.set_unit(unit);
         vector<ll> va, vb;
+        vector<ll> ra, rb;
         for (int _rr = 0; _rr < rep2; _rr++) {
           ll a = rand.range(-15, 16);
           ll b = rand.range(-100, 100);
-          lct.add_line(a, b);
           va.push_back(a);
           vb.push_back(b);
+          if (rand.range(0, 2) == 0) {
+            lct.add_line(a, b);
+            ra.push_back(rngmin);
+            rb.push_back(rngmax);
+          }else {
+            ll ta = rngmin + rand.range(0, rngmax - rngmin + 1);
+            ll tb = ta + rand.range(0, rngmax - ta + 1);
+            lct.add_segment(a, b, ta, tb);
+            ra.push_back(ta);
+            rb.push_back(tb);
+          }
           ll t = rand.range(rngmin, rngmax + 1);
-          ll nv = va[0] * t + vb[0];
-          for (int i = 1; i < ssize(va); i++) {
-            ll w = va[i] * t + vb[i];
-            if (comp(w, nv)) nv = w;
+          ll nv = unit;
+          for (int i = 0; i < ssize(va); i++) {
+            if (ra[i] <= t and t <= rb[i]) {
+              ll w = va[i] * t + vb[i];
+              if (comp(w, nv)) nv = w;
+            }
           }
           assert(lct.query(t) == nv);
         }
       }
     };
-    func(less<ll>());
-    func(greater<ll>());
+    func(less<ll>(), big);
+    func(greater<ll>(), -big);
   }
 
 
